38d74a5f249150d2aed87d286556738024768153
Matt Travi <programmer@travi.org>
2020-09-06 21:49:35 -0500

    feat(project-config): created the eslintrc for the project

--- src/scaffold-test.js
+++ src/scaffold-test.js
@@ -1,13 +1,35 @@
 import {assert} from 'chai';
+import sinon from 'sinon';
 import any from '@travi/any';
+import * as writeYaml from '../thirdparty-wrappers/write-yaml';
 import scaffold from './scaffold';
 
 suite('scaffold', () => {
+  let sandbox;
+  const projectRoot = any.string();
+
+  setup(() => {
+    sandbox = sinon.createSandbox();
+
+    sandbox.stub(writeYaml, 'default');
+  });
+
+  teardown(() => sandbox.restore());
+
   test('that the chosen form8ion config is extended', async () => {
     const scope = any.word();
+    writeYaml.default.resolves();
 
-    const {scripts, devDependencies} = await scaffold({scope});
+    const {scripts, devDependencies} = await scaffold({projectRoot, scope});
 
+    assert.calledWith(
+      writeYaml.default,
+      `${projectRoot}/.eslintrc.yml`,
+      {
+        root: true,
+        extends: [`@${scope}`, '.']
+      }
+    );
     assert.deepEqual(scripts, {'lint:js': 'eslint .'});
     assert.deepEqual(devDependencies, [`@${scope}/eslint-config`]);
   });
--- src/scaffold.js
+++ src/scaffold.js
@@ -1,4 +1,18 @@
-export default function ({scope}) {
+import writeYaml from '../thirdparty-wrappers/write-yaml';
+
+export default async function ({projectRoot, scope}) {
+  // install form8ion config of the same context as a prod dependency (save-exact) (needs chosen context)
+  // write .eslintrc.yml (needs scope)
+  // write index to extend form8ion config of the same context (needs chosen context)
+
+  await writeYaml(
+    `${projectRoot}/.eslintrc.yml`,
+    {
+      root: true,
+      extends: [`@${scope}`, '.']
+    }
+  );
+
   return {
     scripts: {'lint:js': 'eslint .'},
     devDependencies: [`@${scope}/eslint-config`]
